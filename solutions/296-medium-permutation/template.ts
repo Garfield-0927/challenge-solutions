00638a06c41e59dfc523f71a15ecfc899da30ca9
Garfield-0927 <929780650@qq.com>
2022-06-22 00:18:11 +0800

    delete some variables

--- solutions/296-medium-permutation/template.ts
+++ solutions/296-medium-permutation/template.ts
@@ -5,7 +5,3 @@ type Permutation<T, U = T> = isNever<T> extends true
 	: T extends `${infer P}`
 	? [P, ...Permutation<Exclude<U, P>>]
 	: [];
-
-let b: isNever<false>;
-let a: Permutation<"A" | "B" | "C">;
-let aa: Permutation<"B" | "C">;
